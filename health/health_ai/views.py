b59add088af13e237f41836d8da0aabe743dacd4
Erick Pinheiro <e.stefene.pinheiro@gmail.com>
2025-02-02 22:31:21 -0300

    sistema implementado, front e back inseridos, falta implementar a IA

--- health/health_ai/views.py
+++ health/health_ai/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def chat(request):
+    pass
 
 # Create your views here.
