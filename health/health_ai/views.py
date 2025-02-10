8b3d9a8f2118d2796b3e089ce57ed9772ff00f5d
Erick Pinheiro <e.stefene.pinheiro@gmail.com>
2025-02-09 22:03:18 -0300

    PROJETO FUNCIONANDO

--- health/health_ai/views.py
+++ health/health_ai/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 from .models import Message
 
 
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def chat(request):
     if request.method == "POST":
         try:
@@ -15,11 +17,11 @@ def chat(request):
 
             if message:
                 Message.objects.create(content=message)
-                return JsonResponse('Mensagem recebida com sucesso!', status=200)
+                return JsonResponse('Mensagem recebida com sucesso!', safe=False, status=200)
             else:
-                return JsonResponse('Mensagem vazia', status=400)
+                return JsonResponse('Mensagem vazia', safe=False, status=400)
         except json.JSONDecodeError:
-            return JsonResponse('Erro ao decodificar JSON', status=400)
+            return JsonResponse('Erro ao decodificar JSON', safe=False, status=400)
     return render(request, 'index.html')
 
 # Create your views here.
